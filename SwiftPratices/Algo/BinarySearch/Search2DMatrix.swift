96e423162a048c0af4c35e2572a5455f9b884f58
Hao Zheng <babyshung8@gmail.com>
2016-06-15 22:09:04 -0400

    search2d matrix

---
+++ SwiftPratices/Algo/BinarySearch/Search2DMatrix.swift
@@ -0,0 +1,49 @@
+
+class Search2DMatrix {
+    func search(matrix: [[Int]]?, target: Int) -> Bool {
+        guard let mtx = matrix where mtx.count > 0 else {
+            return false
+        }
+        
+        var start = 0, end = mtx.count - 1
+        
+        while start + 1 < end {
+            let mid = start + (end - start) / 2
+            if mtx[mid][0] == target {
+                return true
+            } else if mtx[mid][0] > target {
+                end = mid
+            } else {
+                start = mid
+            }
+        }
+        var row: Int
+        if mtx[end][0] <= target {
+            row = end
+        } else if mtx[start][0] <= target {
+            row = start
+        } else {
+            return false
+        }
+        
+        start = 0
+        end = mtx[row].count - 1
+        
+        while start + 1 < end {
+            let mid = start + (end - start) / 2
+            if mtx[row][mid] == target {
+                return true
+            } else if mtx[row][mid] > target {
+                end = mid
+            } else {
+                start = mid
+            }
+        }
+        
+        if mtx[row][start] == target || mtx[row][end] == target {
+            return true
+        } else {
+            return false
+        }
+    }
+}
---
+++ SwiftPraticesTests/Algo/BinarySearch/Search2DMatrixTests.swift
@@ -0,0 +1,60 @@
+
+import XCTest
+@testable import SwiftPratices
+class Search2DMatrixTests: XCTestCase {
+
+    let instance = Search2DMatrix()
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testNil() {
+        XCTAssertFalse(instance.search(nil, target: 2))
+    }
+    
+    func testSmall() {
+        let matrix = [
+            [0, 1],
+            [7, 11]
+        ]
+        XCTAssertFalse(instance.search(matrix, target: -1))
+        XCTAssertFalse(instance.search(matrix, target: 6))
+        XCTAssertFalse(instance.search(matrix, target: 20))
+        
+        XCTAssertTrue(instance.search(matrix, target: 0))
+        XCTAssertTrue(instance.search(matrix, target: 1))
+        XCTAssertTrue(instance.search(matrix, target: 7))
+        XCTAssertTrue(instance.search(matrix, target: 11))
+    }
+    
+    func testNormal() {
+        let matrix = [
+            [1, 3, 4, 6],
+            [8, 11, 20, 22],
+            [30, 40, 100, 101]
+        ]
+        XCTAssertFalse(instance.search(matrix, target: 0))
+        XCTAssertFalse(instance.search(matrix, target: 2))
+        XCTAssertFalse(instance.search(matrix, target: 103))
+        
+        XCTAssertTrue(instance.search(matrix, target: 1))
+        XCTAssertTrue(instance.search(matrix, target: 11))
+        XCTAssertTrue(instance.search(matrix, target: 40))
+        XCTAssertTrue(instance.search(matrix, target: 101))
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
