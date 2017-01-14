25ff15cc6286e8af475b3baac2f28f04076917a6
Hao Zheng <babyshung8@gmail.com>
2017-01-14 11:14:42 -0500

    added strstr daozhang

--- Playgrounds/Strstr.playground/Contents.swift
+++ Playgrounds/Strstr.playground/Contents.swift
@@ -32,6 +32,36 @@ func strStr(_ source: String, _ target: String) -> Int {
 
 let index = strStr("aaa", "aaaa")
 
+func strStrDaoZhang(haystack: String, _ needle: String) -> Int {
+    var hChars = [Character](haystack.characters)
+    var nChars = [Character](needle.characters)
+    
+    guard hChars.count >= nChars.count else {
+        return -1
+    }
+    if nChars.count == 0 {
+        return 0
+    }
+    
+    for i in 0 ... hChars.count - nChars.count {
+        guard hChars[i] == nChars[0] else {
+            continue
+        }
+        
+        for j in 0 ..< nChars.count {
+            guard hChars[i + j] == nChars[j] else {
+                break
+            }
+            if j == nChars.count - 1 {
+                return i
+            }
+        }
+    }
+    
+    return -1
+}
+
+
 //not passed yet
 func strStr2(_ source: String, _ target: String) -> Int {
 
