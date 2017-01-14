da72934b8f27294466c9a612ad21b28b3d48fd9c
Hao Zheng <babyshung8@gmail.com>
2017-01-13 21:20:01 -0500

    fpoi

--- Playgrounds/BinarySearch.playground/Contents.swift
+++ Playgrounds/BinarySearch.playground/Contents.swift
@@ -56,3 +56,25 @@ func sqrt(x: Int) -> Int{
         return Int(start)
     }
 }
+
+func firstPosition(nums: [Int], target: Int) -> Int {
+    guard nums.count > 0 else {
+        return -1
+    }
+    var start = 0, end = nums.count - 1
+    while start + 1 < end {
+        let mid = start + (end - start) / 2
+        if nums[mid] >= target {
+            end = mid
+        } else {
+            start = mid
+        }
+    }
+    if nums[start] == target {
+        return start
+    } else if nums[end] == target {
+        return end
+    } else {
+        return -1
+    }
+}
