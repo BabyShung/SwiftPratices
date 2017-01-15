bd37f0c7f72d57d07020ede2fc8ee6c552fa4c4b
Hao Zheng <babyshung8@gmail.com>
2017-01-15 14:59:08 -0500

    sybset2

--- Playgrounds/subsetDFS.playground/Contents.swift
+++ Playgrounds/subsetDFS.playground/Contents.swift
@@ -34,3 +34,34 @@ private func dfsHelper(nums:[Int],
         result.removeLast()
     }
 }
+
+func subsetsII(_ nums: [Int]) -> [[Int]] {
+    guard nums.count > 0 else {
+        return []
+    }
+    
+    let sortedNums = nums.sorted()
+    var current: [Int] = []
+    var results: [[Int]] = []
+    dfsHelperII(nums: sortedNums, index: 0, result: &current, results: &results)
+    return results
+}
+
+private func dfsHelperII(nums:[Int],
+                       index: Int,
+                       result: inout [Int],
+                       results: inout [[Int]]) {
+
+    results.append(result)
+    for i in stride(from: index, to: nums.count, by: 1) {
+        guard i == index || nums[i] != nums[i - 1] else {
+            continue
+        }
+//        if i != index && nums[i] == nums[i - 1] {
+//            continue
+//        }
+        result.append(nums[i])
+        dfsHelperII(nums: nums, index: i + 1, result: &result, results: &results)
+        result.removeLast()
+    }
+}
