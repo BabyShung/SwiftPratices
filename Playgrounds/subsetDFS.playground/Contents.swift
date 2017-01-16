b84275f1dd372a347bf1c8e03d7180d976ef69eb
Hao Zheng <babyshung8@gmail.com>
2017-01-15 23:05:28 -0500

    combine

--- Playgrounds/subsetDFS.playground/Contents.swift
+++ Playgrounds/subsetDFS.playground/Contents.swift
@@ -21,16 +21,16 @@ private func dfsHelper(nums:[Int],
                        container: inout [Int],
                        results: inout [[Int]]) {
     results.append(container)
-    //print("adding array: \(container)")
+    print("adding array: \(container)")
     for i in stride(from: index, to: nums.count, by: 1) {
         container.append(nums[i])
-        //print("current: \(container)")
+        print("current: \(container)")
         dfsHelper(nums: nums, index: i + 1, container: &container, results: &results)
         container.removeLast()
     }
 }
 
-subsets([1, 2, 3])
+//subsets([1, 2, 3])
 
 func subsetsII(_ nums: [Int]) -> [[Int]] {
     guard nums.count > 0 else {
@@ -56,9 +56,6 @@ private func dfsHelperII(nums:[Int],
             print("skip - i: \(i), index: \(index), nums[\(i)] = \(nums[i]), nums[\(i) - 1] = \(nums[i - 1])")
             continue
         }
-//        if i != index && nums[i] == nums[i - 1] {
-//            continue
-//        }
         container.append(nums[i])
         print("current: \(container)")
         dfsHelperII(nums: nums, index: i + 1, container: &container, results: &results)
@@ -66,7 +63,68 @@ private func dfsHelperII(nums:[Int],
     }
 }
 
-subsetsII([1, 1, 2, 2])
+//subsetsII([1, 1, 2, 2])
+
+
+//permutation
+func permute(_ nums: [Int]) -> [[Int]] {
+    guard nums.count > 0 else {
+        return []
+    }
+    var res: [[Int]] = []
+    var container: [Int] = []
+    permuteHelper(nums, &res, &container)
+    return res
+}
+
+private func permuteHelper(_ nums: [Int],
+                           _ res: inout [[Int]],
+                           _ container: inout [Int]) {
+    if container.count == nums.count {
+        res.append(container)
+//        print("adding array: \(container)")
+    }
+    for i in stride(from: 0, to: nums.count, by: 1) {
+        guard !container.contains(nums[i]) else {
+//            print("skip - current: \(container)")
+            continue
+        }
+        container.append(nums[i])
+//        print("current: \(container)")
+        permuteHelper(nums, &res, &container)
+        container.removeLast()
+    }
+}
+
+//permute([1, 2])
+
+//combination
+func combine(_ n: Int, _ k: Int) -> [[Int]] {
+
+    var res: [[Int]] = []
+    var container: [Int] = []
+    cmnHelper(&res, &container, n, k, 1)
+    return res
+}
+
+private func cmnHelper(_ res: inout [[Int]],
+                       _ container: inout [Int],
+                       _ n: Int,
+                       _ k: Int,
+                       _ pos: Int) {
+    if container.count == k {
+        res.append(container)
+//        print("adding array: \(container)")
+        return
+    }
+    for i in stride(from: pos, through: n, by: 1) {
+        container.append(i)
+        cmnHelper(&res, &container, n, k, i + 1)
+        container.removeLast()
+    }
+}
+
+//combine(20, 1)
 
 //combination sum
 func combinationSum(_ candidates: [Int], _ target: Int) -> [[Int]] {
@@ -86,15 +144,15 @@ private func comDFSHelper(_ candidates: inout [Int], _ target: Int, res: inout [
     }
     if target == 0 {
         res.append(container)
-        //print("adding array: \(container)")
+        print("adding array: \(container)")
     } else {
         for i in stride(from: index, to: candidates.count, by: 1) {
             container.append(candidates[i])
-            //print("current: \(container)")
+            print("current: \(container)")
             comDFSHelper(&candidates, target - candidates[i], res: &res, container: &container , index: i)
             container.removeLast()
         }
     }
 }
 
-combinationSum([2, 3, 6, 7], 7)
+//combinationSum([2, 3, 6, 7], 7)
