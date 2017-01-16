b4f7370165be230daadb21e0208199fb4c4ce049
Hao Zheng <babyshung8@gmail.com>
2017-01-15 22:09:57 -0500

    good testing

--- Playgrounds/subsetDFS.playground/Contents.swift
+++ Playgrounds/subsetDFS.playground/Contents.swift
@@ -20,20 +20,17 @@ private func dfsHelper(nums:[Int],
                        index: Int,
                        container: inout [Int],
                        results: inout [[Int]]) {
-    //[1, 2, 3]
-    //[]
-    //[1]
-    //[1, 2]
-    //[1, 2, 3]
-    //
-    
     results.append(container)
+    //print("adding array: \(container)")
     for i in stride(from: index, to: nums.count, by: 1) {
         container.append(nums[i])
+        //print("current: \(container)")
         dfsHelper(nums: nums, index: i + 1, container: &container, results: &results)
         container.removeLast()
     }
 }
+
+subsets([1, 2, 3])
 
 func subsetsII(_ nums: [Int]) -> [[Int]] {
     guard nums.count > 0 else {
@@ -53,18 +50,23 @@ private func dfsHelperII(nums:[Int],
                        results: inout [[Int]]) {
 
     results.append(container)
+    print("adding array: \(container)")
     for i in stride(from: index, to: nums.count, by: 1) {
         guard i == index || nums[i] != nums[i - 1] else {
+            print("skip - i: \(i), index: \(index), nums[\(i)] = \(nums[i]), nums[\(i) - 1] = \(nums[i - 1])")
             continue
         }
 //        if i != index && nums[i] == nums[i - 1] {
 //            continue
 //        }
         container.append(nums[i])
+        print("current: \(container)")
         dfsHelperII(nums: nums, index: i + 1, container: &container, results: &results)
         container.removeLast()
     }
 }
+
+subsetsII([1, 1, 2, 2])
 
 //combination sum
 func combinationSum(_ candidates: [Int], _ target: Int) -> [[Int]] {
@@ -84,11 +86,15 @@ private func comDFSHelper(_ candidates: inout [Int], _ target: Int, res: inout [
     }
     if target == 0 {
         res.append(container)
+        //print("adding array: \(container)")
     } else {
         for i in stride(from: index, to: candidates.count, by: 1) {
             container.append(candidates[i])
+            //print("current: \(container)")
             comDFSHelper(&candidates, target - candidates[i], res: &res, container: &container , index: i)
             container.removeLast()
         }
     }
 }
+
+combinationSum([2, 3, 6, 7], 7)
