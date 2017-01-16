02eff788c4520001b2a950d1042fb040b16b6f55
Hao Zheng <babyshung8@gmail.com>
2017-01-16 11:52:45 -0500

    so freaking subtle

--- Playgrounds/subsetDFS.playground/Contents.swift
+++ Playgrounds/subsetDFS.playground/Contents.swift
@@ -4,15 +4,15 @@ import UIKit
 
 var str = "Hello, playground"
 
-private func OUTPUT_ADDING_ARR(_ container: [Int], _ pos: Int) {
-    print("adding list: \(container), pos: \(pos)")
+private func OUTPUT_ADDING_ARR(_ container: [Int], _ start: Int) {
+    print("adding list: \(container), start: \(start)")
 }
 
 private func OUTPUT_ADDING_ELEMENT(_ container: [Int], _ i: Int, _ nums: [Int]) {
     if nums.count > 0 {
-        print("added: num[\(i)]: \(nums[i]), container: \(container), next i: \(i + 1)")
+        print("added: num[\(i)]: \(nums[i]), container: \(container), next i(start): \(i + 1)")
     } else {
-        print("added: \(i), container: \(container), next i: \(i + 1)")
+        print("added: \(i), container: \(container), next i(start): \(i + 1)")
     }
 }
 
@@ -25,9 +25,22 @@ private func OUTPUT_END_FORLOOP() {
     print("--- for loop done ---")
 }
 
-private func OUTPUT_CONTINUE(_ i: Int, _ pos: Int, _ nums: [Int]) {
-    print("** continued - i: \(i), pos: \(pos), nums[\(i)] = \(nums[i]) - next i: \(i + 1)")
+private func OUTPUT_CONTINUE(_ i: Int, _ start: Int, _ nums: [Int]) {
+    print("** continued - i: \(i), start: \(start), nums[\(i)] = \(nums[i]), nums[\(i - 1)] = \(nums[i - 1]) - next i: \(i + 1)")
     //, nums[\(i) - 1] = \(nums[i - 1])
+}
+
+private func OUTPUT_I_START(_ i: Int, _ start: Int) {
+    print("i: \(i), start: \(start)")
+    //, nums[\(i) - 1] = \(nums[i - 1])
+}
+
+private func OUTPUT_SUM_RETURN(_ container: [Int], _ target: Int, _ i: Int, _ candidates: [Int]) {
+    print("return - no need to check ascending - container: \(container), target: \(target), candidates[\(i)]: \(candidates[i])")
+}
+
+private func OUTPUT_SUM_REMOVED_RETURN(_ container: [Int], _ target: Int, _ i: Int, _ candidates: [Int]) {
+    print("function returned - removedLast: \(container), target: \(target), candidates[\(i)]: \(candidates[i])")
 }
 /////////////
 
@@ -46,12 +59,12 @@ func subsets(_ nums: [Int]) -> [[Int]] {
 }
 
 private func dfsHelper(_ nums:[Int],
-                       _ pos: Int,
+                       _ start: Int,
                        _ container: inout [Int],
                        _ results: inout [[Int]]) {
     results.append(container)
-    print("adding array: \(container), cur pos: \(pos)")
-    for i in stride(from: pos, to: nums.count, by: 1) {
+    OUTPUT_ADDING_ARR(container, start)
+    for i in stride(from: start, to: nums.count, by: 1) {
         container.append(nums[i])
         OUTPUT_ADDING_ELEMENT(container, i, nums)
         dfsHelper(nums, i + 1, &container, &results)
@@ -76,15 +89,15 @@ func subsetsWithDup(_ nums: [Int]) -> [[Int]] {
 
 
 private func dfsHelperII(_ nums:[Int],
-                         _ pos: Int,
+                         _ start: Int,
                          _ container: inout [Int],
                          _ results: inout [[Int]]) {
 
     results.append(container)
-    OUTPUT_ADDING_ARR(container, pos)
-    for i in stride(from: pos, to: nums.count, by: 1) {
-        guard i == pos || nums[i] != nums[i - 1] else {
-            OUTPUT_CONTINUE(i, pos, nums)
+    OUTPUT_ADDING_ARR(container, start)
+    for i in stride(from: start, to: nums.count, by: 1) {
+        guard i == start || nums[i] != nums[i - 1] else {
+            OUTPUT_CONTINUE(i, start, nums)
             continue
         }
         container.append(nums[i])
@@ -147,13 +160,13 @@ private func cmnHelper(_ res: inout [[Int]],
                        _ container: inout [Int],
                        _ n: Int,
                        _ k: Int,
-                       _ pos: Int) {
+                       _ start: Int) {
     if container.count == k {
         res.append(container)
-        OUTPUT_ADDING_ARR(container, pos)
+        OUTPUT_ADDING_ARR(container, start)
         return
     }
-    for i in stride(from: pos, through: n, by: 1) {
+    for i in stride(from: start, through: n, by: 1) {
         container.append(i)
         OUTPUT_ADDING_ELEMENT(container, i, [])
         cmnHelper(&res, &container, n, k, i + 1)
@@ -163,7 +176,7 @@ private func cmnHelper(_ res: inout [[Int]],
     OUTPUT_END_FORLOOP()
 }
 
-combine(20, 1)
+//combine(4, 2)
 
 //combination sum
 func combinationSum(_ candidates: [Int], _ target: Int) -> [[Int]] {
@@ -181,20 +194,22 @@ private func comDFSHelper(_ candidates: inout [Int],
                           _ target: Int,
                           _ res: inout [[Int]],
                           _ container: inout [Int],
-                          _ index: Int) {
+                          _ start: Int) {
     if target == 0 {
         res.append(container)
-        print("adding array: \(container)")
+        OUTPUT_ADDING_ARR(container, start)
     } else {
-        for i in stride(from: index, to: candidates.count, by: 1) {
+        for i in stride(from: start, to: candidates.count, by: 1) {
             guard target >= candidates[i] else {//since it's ascending
-                print("skip - target: \(target), candidates[\(i)]: \(candidates[i])")
+                OUTPUT_SUM_RETURN(container, target, i, candidates)
                 return
             }
             container.append(candidates[i])
-            print("current: \(container), target: \(target), candidates[\(i)]: \(candidates[i])")
+            OUTPUT_ADDING_ELEMENT(container, i, candidates)
             comDFSHelper(&candidates, target - candidates[i], &res, &container , i)
             container.removeLast()
+            OUTPUT_SUM_REMOVED_RETURN(container, target, i, candidates)
+
         }
     }
 }
@@ -218,31 +233,31 @@ private func comDFSHelper2(_ candidates: inout [Int],
                           _ target: Int,
                           _ res: inout [[Int]],
                           _ container: inout [Int],
-                          _ index: Int) {
+                          _ start: Int) {
 
     if target == 0 {
         res.append(container)
-        print("** adding array: \(container)")
+        OUTPUT_ADDING_ARR(container, start)
     } else {
-        var prev = -1
-        for i in stride(from: index, to: candidates.count, by: 1) {
-            guard candidates[i] != prev else {
-                print("What - prev: \(prev), candidates[\(i)]: \(candidates[i])")
-
+        for i in stride(from: start, to: candidates.count, by: 1) {
+            guard i == start || candidates[i] != candidates[i - 1] else {
+                OUTPUT_CONTINUE(i, start, candidates)
                 continue
             }
+            OUTPUT_I_START(i, start)
             guard target >= candidates[i] else {//since it's ascending
-                print("skip - target: \(target), candidates[\(i)]: \(candidates[i])")
+                OUTPUT_SUM_RETURN(container, target, i, candidates)
                 return
             }
             container.append(candidates[i])
-            print("current: \(container), target: \(target), candidates[\(i)]: \(candidates[i])")
+            OUTPUT_ADDING_ELEMENT(container, i, candidates)
             comDFSHelper2(&candidates, target - candidates[i], &res, &container , i + 1)
-            prev = candidates[i]
             container.removeLast()
+            OUTPUT_SUM_REMOVED_RETURN(container, target, i, candidates)
+
         }
     }
 }
 
 //[1, 1, 2, 5, 6, 7, 10]
-//combinationSum2([10, 1, 2, 7, 6, 1, 5], 8)
+combinationSum2([1, 1, 1, 5], 8)
