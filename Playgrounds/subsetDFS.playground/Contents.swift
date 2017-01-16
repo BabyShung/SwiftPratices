360696c2f824971c4b2ba3528dd2b737db905eca
Hao Zheng <babyshung8@gmail.com>
2017-01-15 21:29:46 -0500

    combination sum ---

--- Playgrounds/subsetDFS.playground/Contents.swift
+++ Playgrounds/subsetDFS.playground/Contents.swift
@@ -34,3 +34,31 @@ private func dfsHelper(nums:[Int],
         result.removeLast()
     }
 }
+
+//combination sum
+func combinationSum(_ candidates: [Int], _ target: Int) -> [[Int]] {
+    guard candidates.count > 0 else {
+        return []
+    }
+    var sorted = candidates.sorted()
+    var container: [Int] = []
+    var res: [[Int]] = []
+    comDFSHelper(&sorted, target, res: &res, container: &container, index: 0)
+    return res
+}
+
+private func comDFSHelper(_ candidates: inout [Int], _ target: Int, res: inout [[Int]], container: inout [Int], index: Int) {
+    guard target >= 0 else {
+        return
+    }
+    if target == 0 {
+        res.append(container)
+    } else {
+        for i in stride(from: index, to: candidates.count, by: 1) {
+            container.append(candidates[i])
+            comDFSHelper(&candidates, target - candidates[i], res: &res, container: &container , index: i)
+            container.removeLast()
+        }
+    }
+}
+
