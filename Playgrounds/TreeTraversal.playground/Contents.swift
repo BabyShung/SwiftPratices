8b2783badd81e440058625210fa8b41e22013a59
Hao Zheng <babyshung8@gmail.com>
2017-01-22 11:51:39 -0500

    more traversals

--- Playgrounds/TreeTraversal.playground/Contents.swift
+++ Playgrounds/TreeTraversal.playground/Contents.swift
@@ -4,6 +4,36 @@ import UIKit
 
 var str = "Hello, playground"
 
+/*
+ Stack
+ 
+ Push and pop are O(1) operations.
+ */
+
+public struct Stack<T> {
+    fileprivate var array = [T]()
+    
+    public var count: Int {
+        return array.count
+    }
+    
+    public var isEmpty: Bool {
+        return array.isEmpty
+    }
+    
+    public mutating func push(_ element: T) {
+        array.append(element)
+    }
+    
+    public mutating func pop() -> T? {
+        return array.popLast()
+    }
+    
+    public var top: T? {
+        return array.last
+    }
+}
+
 public class TreeNode {
     public var val: Int
     public var left: TreeNode?
@@ -15,6 +45,7 @@ public class TreeNode {
     }
 }
 
+//preorder
 func preorderTraversal(_ root: TreeNode?) -> [Int] {
     var res: [Int] = []
     helper(root, &res)
@@ -30,3 +61,54 @@ private func helper(_ cur: TreeNode?, _ res: inout [Int]) {
     helper(cur.right, &res)
 }
 
+func preorderTraversalIterative(_ root: TreeNode?) -> [Int] {
+    guard let root = root else {
+        return []
+    }
+    var res: [Int] = []
+    var stack = Stack<TreeNode>()
+    stack.push(root)
+    while !stack.isEmpty {
+        let cur = stack.pop()
+        if let right = cur?.right {
+            stack.push(right)
+        }
+        if let left = cur?.left {
+            stack.push(left)
+        }
+        res.append(cur!.val);
+    }
+    return res
+}
+
+//inorder
+func inorderTraversal(_ root: TreeNode?) -> [Int] {
+    var res: [Int] = []
+    helperInorder(root, &res)
+    return res
+}
+
+private func helperInorder(_ cur: TreeNode?, _ res: inout [Int]) {
+    guard let cur = cur else {
+        return
+    }
+    helperInorder(cur.left, &res)
+    res.append(cur.val)
+    helperInorder(cur.right, &res)
+}
+
+//postorder
+func postorderTraversal(_ root: TreeNode?) -> [Int] {
+    var res: [Int] = []
+    helperPost(root, &res)
+    return res
+}
+
+private func helperPost(_ cur: TreeNode?, _ res: inout [Int]) {
+    guard let cur = cur else {
+        return
+    }
+    helperPost(cur.left, &res)
+    helperPost(cur.right, &res)
+    res.append(cur.val)
+}
