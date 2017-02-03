30c49035d314f0ca350e0c6358ce310e2bf64660
Hao Zheng <babyshung8@gmail.com>
2017-02-03 17:57:41 -0500

    added

--- JavaCode/src/LC/LinkedList.java
+++ JavaCode/src/LC/LinkedList.java
@@ -75,4 +75,101 @@ public class LinkedList {
         tail.next = null;
         return dummy.next;
     }
+
+    //http://www.lintcode.com/en/problem/insert-into-a-cyclic-sorted-list/
+    public ListNode insert(ListNode node, int x) {
+        // Write your code here
+        if (node == null) {
+            node = new ListNode(x);
+            node.next = node;
+            return node;
+        }
+
+        ListNode p = node;
+        ListNode prev = null;
+        do {
+            prev = p;
+            p = p.next;
+            if (x <= p.val && x >= prev.val) {
+                break;
+            }
+            if ((prev.val > p.val) && (x < p.val || x > prev.val)) {
+                break;
+            }
+        } while (p != node);
+
+        ListNode newNode = new ListNode(x);
+        newNode.next = p;
+        prev.next = newNode;
+        return newNode;
+    }
+
+    //http://www.lintcode.com/en/problem/merge-two-sorted-lists/
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        // write your code here
+        ListNode head = new ListNode(-1);
+        ListNode current = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+        if (l1 == null) {
+            current.next = l2;
+        } else {
+            current.next = l1;
+        }
+        return head.next;
+    }
+
+    //http://www.lintcode.com/en/problem/sort-list/
+    private ListNode findMiddle(ListNode head) {
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    private ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while (head1 != null && head2 != null) {
+            if (head1.val < head2.val) {
+                tail.next = head1;
+                head1 = head1.next;
+            } else {
+                tail.next = head2;
+                head2 = head2.next;
+            }
+            tail = tail.next;
+        }
+        if (head1 != null) {
+            tail.next = head1;
+        } else {
+            tail.next = head2;
+        }
+
+        return dummy.next;
+    }
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode mid = findMiddle(head);
+
+        ListNode right = sortList(mid.next);
+        mid.next = null;
+        ListNode left = sortList(head);
+
+        return merge(left, right);
+    }
 }
