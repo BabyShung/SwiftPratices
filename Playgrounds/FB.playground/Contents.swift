c498fcc23ae65d6a969d9258003e34f79b5358ec
Hao Zheng <babyshung8@gmail.com>
2017-02-06 17:30:51 -0800

    move zeros

---
+++ Playgrounds/FB.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+func moveZeroes(_ nums: inout [Int]) {
+    guard nums.count > 0 else {
+        return
+    }
+    var last = 0
+    for i in stride(from: 0, to: nums.count, by: 1) {
+        if nums[i] != 0 {
+            if (last != i) {
+                swap(&nums[i], &nums[last])
+            }
+            last += 1
+        }
+    }
+}
+
+var arr = [1, 2, 0, 2, 0, 3]
+moveZeroes(&arr)
+print(arr)
